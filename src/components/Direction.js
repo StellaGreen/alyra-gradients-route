b6c99cfe2f60f383508f4defc89290e9de077b5f
Benmissi-A <aminebenmissi@hotmail.fr>
2021-06-01 00:20:55 +0200

    direction fixed

--- src/components/Direction.js
+++ src/components/Direction.js
@@ -7,27 +7,27 @@ const Direction = () => {
   };
   return (
     <>
-    <div className="input-group mb-3 col-sm-6">
       
-      <label className="input-group-text" htmlFor="direction">
-        Direction
-      </label>
-      <select
-        className="form-select"
-        id="direction"
-        value={direction}
-        onChange={handleDirectionChange}
-      >
-        <option value="to right">Right</option>
-        <option value="to right top">Right Top</option>
-        <option value="to right bottom">Right Bottom</option>
-        <option value="to left">Left</option>
-        <option value="to left top">Left Top</option>
-        <option value="to left bottom">Left Bottom</option>
-        <option value="to top">Top</option>
-        <option value="to bottom">Bottom</option>
-      </select>
-      </div>
+       
+          <label className="input-group-text" htmlFor="direction">
+            Direction
+          </label>
+          <select
+            className="form-select"
+            id="direction"
+            value={direction}
+            onChange={handleDirectionChange}
+          >
+            <option value="to right">Right</option>
+            <option value="to right top">Right Top</option>
+            <option value="to right bottom">Right Bottom</option>
+            <option value="to left">Left</option>
+            <option value="to left top">Left Top</option>
+            <option value="to left bottom">Left Bottom</option>
+            <option value="to top">Top</option>
+            <option value="to bottom">Bottom</option>
+          </select>
+   
     </>
   );
 };
