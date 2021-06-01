04af23f8ac11e59cb92ccc3670c7071322204750
Dzheremilz <trempejeremie@gmail.com>
2021-06-01 10:09:19 +0200

    button ff / panda roux mignon

--- src/pages/Proposition.js
+++ src/pages/Proposition.js
@@ -1,20 +1,20 @@
 import GradientPill from "../components/GradientPill";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import ToggleModeButton  from "../components/ToggleModeButton"
+import ToggleModeButton from "../components/ToggleModeButton";
 
 const Proposition = () => {
   const [darkMode, setDarkMode] = useState(true);
   const [color, setColor] = useState({
     start: { r: 0, g: 0, b: 0 },
-    end: { r: 0, g: 0, b: 0 }
-  })
+    end: { r: 0, g: 0, b: 0 },
+  });
   const [name, setName] = useState("default name");
   const { start, end } = color;
   const modeClasses = darkMode ? "bg-dark text-white" : "bg-light";
   const superStyle = {
-    width : "25rem"
-  }
+    width: "25rem",
+  };
 
   const handleChange = (e) => {
     const [ent, val] = e.target.id.split("-");
@@ -42,21 +42,22 @@ const Proposition = () => {
         tags: [],
       }),
     });
-  }
+  };
   const handleReset = () => {
     setColor({
       start: { r: 0, g: 0, b: 0 },
       end: { r: 0, g: 0, b: 0 },
-    })}
+    });
+  };
   return (
     <>
       <ToggleModeButton darkMode={darkMode} setDarkMode={setDarkMode} />
       <div className={`${modeClasses} min-vh-100`}>
         <div className="container">
-          <h1 className="display-2 text-center m-2 p-2">
+          <h1 className="display-2 text-center m-2 p-2 mt-lg-5">
             Make your Own proposition !
           </h1>
-          <h3 className="text-center pb-2 fw-lighter">
+          <h3 className="text-center pb-2 fw-lighter mt-lg-4">
             It's easy to do with the power of RGB !
           </h3>
           <div className="row">
@@ -91,7 +92,6 @@ const Proposition = () => {
             ></input>
           </div>
           <div className="row ">
-          
             <div className="offset-lg-3 col-lg-3 text-center">
               <div className="text-danger">Red Start</div>
               <input
@@ -182,5 +182,5 @@ const Proposition = () => {
       </div>
     </>
   );
-}
-export default Proposition
+};
+export default Proposition;
